26e3b68562861133ad553928fff7beb85b70c1a9
Serhii <serhiinorov110@gmail.com>
2022-08-16 17:32:43 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// const result = confirm('Are you here?');
+// const answer = +prompt('You have 18 y.o.?');
+// const answers = [];
+// answers[0] = prompt('Whats youre name?', '');
+// answers[1] = prompt('Whats youre surname?', '');
+// answers[2] = prompt('Whats old youd?', '');
+
+// const category = 'toys';
+// console.log(`https://norov.com/${category}/5`);
+// const user = 'Serhii';
+// alert(`Hey, ${user}`);
+
+// let incr = 10,
+//     decr = 10;
+
+// incr++;
+// decr--;
+// ++incr;
+// --decr;
+// console.log(++incr);
+// console.log(--decr);
+
+// && - i;
+// || ili;
+
+
